bed023228839f09cb0f5367bddcb4df71ef72c05
Mustafadgny <doganaymustafa44@gmail.com>
2025-08-06 17:42:54 +0300

    password create

--- users/views.py
+++ users/views.py
@@ -3,7 +3,9 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import CustomUser
-from .serializers import UserRegisterSerializer, UserDetailSerializer
+from .serializers import UserRegisterSerializer, UserDetailSerializer, UserDetailSerializer, ChangePasswordSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 # /api/register/ endpoint'i için
 class RegisterView(generics.CreateAPIView):
@@ -21,3 +23,40 @@ class ProfileView(generics.RetrieveUpdateAPIView):
         # Bu fonksiyon, isteği yapan (giriş yapmış) kullanıcının kendisini döndürür.
         # Böylece /api/profile/1 gibi bir ID'ye gerek kalmaz.
         return self.request.user
+
+class ChangePasswordView(generics.UpdateAPIView):
+    """
+    Kullanıcının şifresini değiştirmesi için bir endpoint.
+    """
+    serializer_class = ChangePasswordSerializer
+    model = CustomUser
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({"detail": "Şifre başarıyla güncellendi."}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserListView(generics.ListAPIView):
+    """
+    Tüm kullanıcıları listelemek için bir endpoint.
+    """
+    queryset = CustomUser.objects.all()
+    serializer_class = UserDetailSerializer
+    permission_classes = (IsAuthenticated,) # Sadece giriş yapmışlar listeyi görsün
+
+class UserDetailView(generics.RetrieveAPIView):
+    """
+    ID'ye göre tek bir kullanıcının detayını görmek için bir endpoint.
+    """
+    queryset = CustomUser.objects.all()
+    serializer_class = UserDetailSerializer
+    permission_classes = (IsAuthenticated,)
