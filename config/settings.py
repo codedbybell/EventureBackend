77b8fa981a57f28c0b13fd4e060a01f9b3f2dcc7
Mustafadgny <doganaymustafa44@gmail.com>
2025-08-06 17:40:43 +0300

    user models

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "events",
+    "events1",
     "rest_framework",
     'rest_framework_simplejwt',
     'drf_spectacular',
